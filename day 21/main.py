b8ab87842f6d67e04e9b74f1ac69d3461405d1b7
Abhi Bhaskar <abhibhaskarnav2002@gmail.com>
2024-09-05 21:23:08 +0530

    updating ball with function

--- day 21/main.py
+++ day 21/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 import time
 from paddle import Paddle
+from ball import Ball
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -10,6 +11,7 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
 
 screen.listen()
 
@@ -21,7 +23,19 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    screen.update()
     time.sleep(0.1)
+    screen.update()
+    ball.move()
 
+    # Detecting the collision with the wall
+    if ball.ycor()>280 or ball.ycor()<-280:
+
+        # need to bounce
+        ball.bounce_y()
+
+    # Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # Detect when paddle hits
 screen.exitonclick()
