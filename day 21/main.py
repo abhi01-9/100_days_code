38b120fe2565dd0c943b019d93be0161b6148982
Abhi Bhaskar <abhibhaskarnav2002@gmail.com>
2024-09-06 14:36:23 +0530

    updating ball with function

--- day 21/main.py
+++ day 21/main.py
@@ -2,6 +2,7 @@ from turtle import Screen
 import time
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -12,6 +13,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 
@@ -28,8 +30,7 @@ while game_is_on:
     ball.move()
 
     # Detecting the collision with the wall
-    if ball.ycor()>280 or ball.ycor()<-280:
-
+    if ball.ycor() > 280 or ball.ycor() < -280:
         # need to bounce
         ball.bounce_y()
 
@@ -37,5 +38,14 @@ while game_is_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
-    # Detect when paddle hits
+    # Detect when R paddle hits
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    # Detect when L paddle hits
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
 screen.exitonclick()
--- day 21/scoreboard.py
+++ day 21/scoreboard.py
@@ -10,18 +10,22 @@ class Scoreboard(Turtle):
         self.score = 0
         self.color("white")
         self.penup()
-        self.goto(0, 260)
         self.hideturtle()
+        self.l_score = 0
+        self.r_score = 0
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
-
-    def game_over(self):
-        self.goto(0,0)
-        self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
-
-    def increase_score(self):
-        self.score += 1
         self.clear()
+        self.goto(-100, 200)
+        self.write(self.l_score, align="center", font=("Courier", 80, "normal"))
+        self.goto(100, 200)
+        self.write(self.r_score, align="center", font=("Courier", 80, "normal"))
+
+    def l_point(self):
+        self.l_score += 1
+        self.update_scoreboard()
+
+    def r_point(self):
+        self.r_score += 1
         self.update_scoreboard()
